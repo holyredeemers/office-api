4619e4557e02314c4ee822251ded095059e9452b
Kingswin <kingswin5549@gmail.com>
2025-07-08 14:15:14 +0530

    apiupdate

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: 'localhost',
+  user: 'holyredeemers',
+  password: 'Holy@#0987',
+  database: 'holydatabase',
+  port: 5432,
 
 });
 
